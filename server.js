4f24034415c0754c64565b2f6954ae1c1707962f
Daniotrix <teamdglsoft@gail.com>
2020-05-15 18:43:09 -0500

    qweqwe

--- server.js
+++ server.js
@@ -39,7 +39,6 @@ app.use(function(req, res, next) {
 });
 
 app.use(express.static('www'));
-app.set('port', process.env.PORT || 5000);
-app.listen(app.get('port'), function () {
+app.listen(app.get(process.env.PORT || 5000), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
